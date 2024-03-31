1a4ae22950776148c057c51b0ff1ef210f3cc364
blck-snwmn <whitesnowmancreator@gmail.com>
2024-04-01 01:40:23 +0900

    Add param

--- ip-imggen-pages/app/routes/_index.tsx
+++ ip-imggen-pages/app/routes/_index.tsx
@@ -11,89 +11,49 @@ export const meta: MetaFunction = () => {
 		},
 	];
 };
+
+const FormInput = ({ label, name }: { label: string; name: string }) => {
+	return (
+		<div className="mb-4">
+			<label htmlFor={name} className="block text-gray-700 font-bold mb-2">
+				{label}
+			</label>
+			<input
+				type="text"
+				id={name}
+				name={name}
+				className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+			/>
+		</div>
+	);
+};
+
 const FormPage = () => {
+	const formInputs = [
+		{ label: "アイコン", name: "icon" },
+		{ label: "名前", name: "name" },
+		{ label: "趣味", name: "hobby" },
+		{ label: "好きな食べ物", name: "favoriteFood" },
+		{ label: "好きな映画", name: "favoriteMovie" },
+		{ label: "お気に入りの場所", name: "favoritePlace" },
+	];
+
 	return (
 		<div className="min-h-screen bg-gray-100 flex items-center justify-center">
 			<div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
 				<h2 className="text-2xl font-bold mb-6">フォーム</h2>
-				<form>
-					<div className="mb-4">
-						<label
-							htmlFor="name"
-							className="block text-gray-700 font-bold mb-2"
-						>
-							名前
-						</label>
-						<input
-							type="text"
-							id="name"
-							name="name"
-							className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-						/>
-					</div>
-					<div className="mb-4">
-						<label
-							htmlFor="email"
-							className="block text-gray-700 font-bold mb-2"
-						>
-							メールアドレス
-						</label>
-						<input
-							type="text"
-							id="email"
-							name="email"
-							className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-						/>
-					</div>
-					<div className="mb-4">
-						<label
-							htmlFor="phone"
-							className="block text-gray-700 font-bold mb-2"
-						>
-							電話番号
-						</label>
-						<input
-							type="text"
-							id="phone"
-							name="phone"
-							className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-						/>
-					</div>
-					<div className="mb-4">
-						<label
-							htmlFor="address"
-							className="block text-gray-700 font-bold mb-2"
-						>
-							住所
-						</label>
-						<input
-							type="text"
-							id="address"
-							name="address"
-							className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-						/>
-					</div>
-					<div className="mb-6">
-						<label
-							htmlFor="message"
-							className="block text-gray-700 font-bold mb-2"
-						>
-							メッセージ
-						</label>
-						<input
-							type="text"
-							id="message"
-							name="message"
-							className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-						/>
-					</div>
+				<Form method="POST" action="/download" reloadDocument>
+					{formInputs.map((input, index) => (
+						// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+						<FormInput key={index} label={input.label} name={input.name} />
+					))}
 					<button
 						type="submit"
-						className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-200"
+						className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-200 mt-4 w-full"
 					>
 						送信
 					</button>
-				</form>
+				</Form>
 			</div>
 		</div>
 	);
--- ip-imggen-pages/app/routes/download.tsx
+++ ip-imggen-pages/app/routes/download.tsx
@@ -4,7 +4,36 @@ import { useActionData } from "@remix-run/react";
 export const action = async ({ context, request }: ActionFunctionArgs) => {
 	const formData = await request.formData();
 	const icon = formData.get("icon");
-	const text = formData.get("text");
+	const name = formData.get("name");
+	const hobby = formData.get("hobby");
+	const favoriteFood = formData.get("favoriteFood");
+	const favoriteMovie = formData.get("favoriteMovie");
+	const favoritePlace = formData.get("favoritePlace");
+
+	if (!icon) {
+		console.error("Bad Request: icon is required");
+		return new Response("Bad Request", { status: 400 });
+	}
+	if (!name) {
+		console.error("Bad Request: name is required");
+		return new Response("Bad Request", { status: 400 });
+	}
+	if (!hobby) {
+		console.error("Bad Request: hobby is required");
+		return new Response("Bad Request", { status: 400 });
+	}
+	if (!favoriteFood) {
+		console.error("Bad Request: favoriteFood is required");
+		return new Response("Bad Request", { status: 400 });
+	}
+	if (!favoriteMovie) {
+		console.error("Bad Request: favoriteMovie is required");
+		return new Response("Bad Request", { status: 400 });
+	}
+	if (!favoritePlace) {
+		console.error("Bad Request: favoritePlace is required");
+		return new Response("Bad Request", { status: 400 });
+	}
 
 	const { env } = context.cloudflare;
 	const resp = await env.GENERATOR.fetch("http://localhost:8787", {
@@ -12,7 +41,14 @@ export const action = async ({ context, request }: ActionFunctionArgs) => {
 		headers: {
 			"Content-Type": "application/json",
 		},
-		body: JSON.stringify({ icon, text }),
+		body: JSON.stringify({
+			icon,
+			name,
+			hobby,
+			favoriteFood,
+			favoriteMovie,
+			favoritePlace,
+		}),
 	});
 	if (!resp.ok) {
 		throw new Error(`Failed to fetch: ${await resp.text()}`);
